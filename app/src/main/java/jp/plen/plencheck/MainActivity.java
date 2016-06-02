9de7a43247e6626647e99ba77507e327bee82685
smasashi <love_bz_ultrasoul@yahoo.co.jp>
2016-06-02 17:31:53 +0900

    fix ui ux

--- app/src/main/java/jp/plen/plencheck/MainActivity.java
+++ app/src/main/java/jp/plen/plencheck/MainActivity.java
@@ -1,50 +1,127 @@
 package jp.plen.plencheck;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import jp.plen.plencheck.ble.BLEDevice;
 
 
-public class MainActivity extends ActionBarActivity implements BLEDevice.BLECallbacks{
+public class MainActivity extends ActionBarActivity implements BLEDevice.BLECallbacks {
     private String TAG = "MainActivity";
     private boolean isClearChecked = false;
-    private int checkedNum = R.id.radioButton0;
+    private int checkedNum = 0;
     private BLEDevice bleDevice;
-    private int map[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+    private int map[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 12, 13, 14, 15, 16, 17, 18, 19, 20};
     private int default_position[] = {
-        900, 1050, 1250, 800, 800, 850, 500, 300, 850, 900, 750, 550, 1000, 1000, 950, 1300, 1500, 950
+            -40, 245, 470, -100, -205, 50, 445, 245, -75, 15, -70, -390, 250, 195, -105, -510, -305, 60
     };
+
+    private float JointButtonLocation[][] = {
+            {653, 345},
+            {665, 491},
+            {858, 259},
+            {950, 483},
+            {729, 649},
+            {817, 776},
+            {645, 905},
+            {820, 1096},
+            {707, 1195},
+            {449, 350},
+            {430, 517},
+            {259, 232},
+            {159, 486},
+            {382, 669},
+            {270, 799},
+            {441, 915},
+            {281, 1097},
+            {377, 1184}
+    };
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    private boolean isInRange(double centerX, double centerY, double pushedX, double pushedY, double margin) {
+        return      (centerX+margin >= pushedX && centerX-margin <= pushedX)
+                &&  (centerY+margin >= pushedY && centerY-margin <= pushedY);
+    }
+
+    private int convertToJointNum(double x, double y) {
+        for(int i = 0; i < 18; i++) {
+            if(isInRange(JointButtonLocation[i][0]/1080, JointButtonLocation[i][1]/1296, x, y, 0.05)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final VerticalSeekbar vs = (VerticalSeekbar) findViewById(R.id.vertseek);
+        final SeekBar vs = (SeekBar) findViewById(R.id.seekBar);
         final TextView tv = (TextView) findViewById(R.id.textView);
-        vs.setProgress(default_position[0]);
-        tv.setText(String.valueOf(vs.getProgress() - 900));
+        final ImageView iv = (ImageView) findViewById(R.id.plen);
+
+        vs.setProgress(default_position[0]+900);
+        tv.setText(String.valueOf(vs.getProgress() ));
+
+        iv.setOnTouchListener(
+                new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        switch(event.getAction()){
+                            case MotionEvent.ACTION_DOWN:
+                                Log.d(TAG, String.valueOf(convertToJointNum(event.getX() / iv.getWidth(), event.getY() / iv.getHeight())));
+                                checkedNum = convertToJointNum(event.getX() / iv.getWidth(), event.getY() / iv.getHeight());
+
+                                int value = map[checkedNum];
+                                vs.setProgress(default_position[checkedNum]+900);
+                                tv.setText(String.valueOf(vs.getProgress() -900));
+                                String hexNum = String.format("%02x", value);
+                                String deg = String.format("%03x", (vs.getProgress() -900 ) & 0xFFF);
+                                default_position[checkedNum] = vs.getProgress() -900;
+                                Log.d(TAG, "$an" + hexNum + deg);
+                                bleDevice.write("$an" + hexNum + deg);
+                                break;
+                        }
+                        return false;
+                  }
+              }
+        );
+
+
         vs.setOnSeekBarChangeListener(
-                new VerticalSeekbar.OnSeekBarChangeListener() {
+                new SeekBar.OnSeekBarChangeListener() {
                     @Override
                     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                        tv.setText(String.valueOf(vs.getProgress() - 900));
-                        int i = Integer.parseInt(((RadioButton)findViewById(checkedNum)).getText().toString());
-                        int value = map[i-1];
+                        tv.setText(String.valueOf(vs.getProgress() -900 ));
+                        // now debuging
+                        //int i = Integer.parseInt(((RadioButton)findViewById(checkedNum)).getText().toString());
+                        int i = checkedNum+1;
+                        int value = map[i - 1];
                         String hexNum = String.format("%02x", value);
-                        String deg = String.format("%03x", (vs.getProgress() - 900) & 0xFFF);
-                        default_position[i-1] = vs.getProgress();
+                        String deg = String.format("%03x", (vs.getProgress() -900 ) & 0xFFF);
+                        default_position[i - 1] = vs.getProgress() -900;
                         Log.d(TAG, "$an" + hexNum + deg);
                         bleDevice.write("$an" + hexNum + deg);
                     }
@@ -56,12 +133,13 @@ public class MainActivity extends ActionBarActivity implements BLEDevice.BLECall
 
                     @Override
                     public void onStopTrackingTouch(SeekBar seekBar) {
-                        tv.setText(String.valueOf(vs.getProgress() - 900));
-                        int i = Integer.parseInt(((RadioButton)findViewById(checkedNum)).getText().toString());
-                        int value = map[i-1];
+                        tv.setText(String.valueOf(vs.getProgress() -900 ));
+                        //int i = Integer.parseInt(((RadioButton)findViewById(checkedNum)).getText().toString());
+                        int i = checkedNum+1;
+                        int value = map[i - 1];
                         String hexNum = String.format("%02x", value);
-                        String deg = String.format("%03x", (vs.getProgress() - 900) & 0xFFF);
-                        default_position[i-1] = vs.getProgress();
+                        String deg = String.format("%03x", (vs.getProgress() -900 ) & 0xFFF);
+                        default_position[i - 1] = vs.getProgress() -900;
                         Log.d(TAG, "$an" + hexNum + deg);
                         bleDevice.write("$an" + hexNum + deg);
                     }
@@ -75,14 +153,14 @@ public class MainActivity extends ActionBarActivity implements BLEDevice.BLECall
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-
                         vs.setProgress(vs.getProgress() + 1);
-                        tv.setText(String.valueOf(vs.getProgress() - 900));
-                        int i = Integer.parseInt(((RadioButton)findViewById(checkedNum)).getText().toString());
-                        int value = map[i-1];
+                        tv.setText(String.valueOf(vs.getProgress() -900 ));
+                        //int i = Integer.parseInt(((RadioButton)findViewById(checkedNum)).getText().toString());
+                        int i = checkedNum+1;
+                        int value = map[i - 1];
                         String hexNum = String.format("%02x", value);
-                        String deg = String.format("%03x", (vs.getProgress() - 900) & 0xFFF);
-                        default_position[i-1] = vs.getProgress();
+                        String deg = String.format("%03x", (vs.getProgress() -900 ) & 0xFFF);
+                        default_position[i - 1] = vs.getProgress() -900;
                         Log.d(TAG, "$an" + hexNum + deg);
                         bleDevice.write("$an" + hexNum + deg);
                     }
@@ -94,106 +172,38 @@ public class MainActivity extends ActionBarActivity implements BLEDevice.BLECall
                     @Override
                     public void onClick(View v) {
                         vs.setProgress(vs.getProgress() - 1);
-                        tv.setText(String.valueOf(vs.getProgress() - 900));
-                        int i = Integer.parseInt(((RadioButton)findViewById(checkedNum)).getText().toString());
-                        int value = map[i-1];
+                        tv.setText(String.valueOf(vs.getProgress() -900 ));
+                        //int i = Integer.parseInt(((RadioButton)findViewById(checkedNum)).getText().toString());
+                        int i = checkedNum+1;
+                        int value = map[i - 1];
                         String hexNum = String.format("%02x", value);
-                        String deg = String.format("%03x", (vs.getProgress() - 900) & 0xFFF);
-                        default_position[i-1] = vs.getProgress();
+                        String deg = String.format("%03x", (vs.getProgress() -900 ) & 0xFFF);
+                        default_position[i - 1] = vs.getProgress() -900;
                         Log.d(TAG, "$an" + hexNum + deg);
                         bleDevice.write("$an" + hexNum + deg);
                     }
                 }
         );
 
-        final RadioGroup radioGroupLeft = (RadioGroup) findViewById(R.id.radioGroupLeft);
-        final RadioGroup radioGroupRight = (RadioGroup) findViewById(R.id.radioGroupRight);
-
-        radioGroupLeft.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if(isClearChecked){
-                    isClearChecked = false;
-                } else if (checkedId != -1) {
-                    int i = Integer.parseInt(((RadioButton)findViewById(checkedId)).getText().toString());
-                    int value = map[i-1];
-                    vs.setProgress(default_position[i-1]);
-                    tv.setText(String.valueOf(vs.getProgress() - 900));
-                    String hexNum = String.format("%02x", value);
-                    String deg = String.format("%03x", (vs.getProgress() - 900) & 0xFFF);
-                    Log.d(TAG, "$an" + hexNum + deg);
-                    bleDevice.write("$an" + hexNum + deg);
-
-                    checkedNum = checkedId;
-                    isClearChecked = true;
-                    radioGroupRight.clearCheck();
-                }
-            }
-        });
-
-        radioGroupRight.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if(isClearChecked){
-                    isClearChecked = false;
-                } else if (checkedId != -1) {
-                    int i = Integer.parseInt(((RadioButton)findViewById(checkedId)).getText().toString());
-                    int value = map[i-1];
-                    vs.setProgress(default_position[i-1]);
-                    tv.setText(String.valueOf(vs.getProgress() - 900));
-                    String hexNum = String.format("%02x", value);
-                    String deg = String.format("%03x", (vs.getProgress() - 900) & 0xFFF);
-                    Log.d(TAG, "$an" + hexNum + deg);
-                    bleDevice.write("$an" + hexNum + deg);
-
-                    checkedNum = checkedId;
-                    isClearChecked = true;
-                    radioGroupLeft.clearCheck();
-                }
-            }
-        });
-
         bleDevice = new BLEDevice(this);
         bleDevice.setBLECallbacks(this);
 
-        Button homeButton = (Button)findViewById(R.id.button);
+        Button homeButton = (Button) findViewById(R.id.button);
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int i = Integer.parseInt(((RadioButton)findViewById(checkedNum)).getText().toString());
-                int value = map[i-1];
+                //int i = Integer.parseInt(((RadioButton)findViewById(checkedNum)).getText().toString());
+                int i = checkedNum + 1;
+                int value = map[i - 1];
                 String hexNum = String.format("%02x", value);
-                String deg = String.format("%03x", (vs.getProgress() - 900) & 0xFFF);
+                String deg = String.format("%03x", (vs.getProgress() -900 ) & 0xFFF);
                 Log.d(TAG, ">ho" + hexNum + deg);
-                bleDevice.write(">ho" + hexNum +deg);
+                bleDevice.write(">ho" + hexNum + deg);
             }
         });
-
-        Button maxButton = (Button)findViewById(R.id.button2);
-        maxButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int i = Integer.parseInt(((RadioButton)findViewById(checkedNum)).getText().toString());
-                int value = map[i-1];
-                String hexNum = String.format("%02x", value);
-                String deg = String.format("%03x", (vs.getProgress() - 900) & 0xFFF);
-                Log.d(TAG, ">ma" + hexNum + deg);
-                bleDevice.write(">ma" + hexNum +deg);
-            }
-        });
-
-        Button minButton = (Button)findViewById(R.id.button3);
-        minButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int i = Integer.parseInt(((RadioButton)findViewById(checkedNum)).getText().toString());
-                int value = map[i-1];
-                String hexNum = String.format("%02x", value);
-                String deg = String.format("%03x", (vs.getProgress() - 900) & 0xFFF);
-                Log.d(TAG, ">mi" + hexNum + deg);
-                bleDevice.write(">mi" + hexNum +deg);
-            }
-        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -221,5 +231,45 @@ public class MainActivity extends ActionBarActivity implements BLEDevice.BLECall
     @Override
     public void onConnected(String deviceName) {
         Toast.makeText(this, "Connected " + deviceName, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://jp.plen.plencheck/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://jp.plen.plencheck/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
